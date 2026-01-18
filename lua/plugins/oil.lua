2c13d40c178a00e4d22b36f19d89a3c423cada2b
Yurii Bilous <seidzuro@gmail.com>
2026-01-18 20:08:24 +0200

    oil: adjust config to not show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,40 @@
+local function parse_output(proc)
+    local result = proc:wait()
+    local ret = {}
+    if result.code == 0 then
+        for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+            line = line:gsub("/$", "")
+            ret[line] = true
+        end
+    end
+    return ret
+end
+
+local function new_git_status()
+    return setmetatable({}, {
+        __index = function(self, key)
+            local ignore_proc = vim.system(
+                { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+                {
+                    cwd = key,
+                    text = true,
+                }
+            )
+            local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+                cwd = key,
+                text = true,
+            })
+            local ret = {
+                ignored = parse_output(ignore_proc),
+                tracked = parse_output(tracked_proc),
+            }
+
+            rawset(self, key, ret)
+            return ret
+        end,
+    })
+end
+
 return {
     {
         "stevearc/oil.nvim",
@@ -10,6 +47,19 @@ return {
                     show_hidden = true,
                     is_always_hidden = function(name, bufnr)
                         return name == ".git"
+                    end,
+                    is_hidden_file = function(name, bufnr)
+                        local git_status = new_git_status()
+                        local dir = require("oil").get_current_dir(bufnr)
+                        local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+                        if not dir then
+                            return is_dotfile
+                        end
+                        if is_dotfile then
+                            return not git_status[dir].tracked[name]
+                        else
+                            return git_status[dir].ignored[name]
+                        end
                     end,
                 },
                 win_options = {
