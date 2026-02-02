210cb8d0934b15a1c1d9a2aec6f626f6d332a172
Yurii Bilous <seidzuro@gmail.com>
2026-02-02 22:58:22 +0200

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,76 +1,50 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-context",
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
         build = ":TSUpdate",
+        branch = "main",
+        lazy = false,
+        event = { "BufReadPost", "BufNewFile" },
         config = function()
-            require("nvim-treesitter.install").prefer_git = true
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "python",
-                    "lua",
-                    "c",
-                    "cpp",
-                    "vim",
-                    "json",
-                    "toml",
-                    "yaml",
-                    "rust",
-                    "bitbake",
-                    "cmake",
-                    "bash",
-                    "kotlin",
-                    "go",
-                },
-                sync_install = false,
-                ignore_install = {},
-                modules = {},
-                auto_install = false,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<CR>",
-                        node_incremental = "<CR>",
-                        scope_incremental = false,
-                        node_decremental = "<bs>",
-                    },
-                },
-                textobjects = {
-                    move = {
-                        enable = true,
-                        goto_next_start = { ["]f"] = "@function.outer", ["]o"] = "@class.outer" },
-                        goto_next_end = { ["]F"] = "@function.outer", ["]O"] = "@class.outer" },
-                        goto_previous_start = { ["[f"] = "@function.outer", ["[o"] = "@class.outer" },
-                        goto_previous_end = { ["[F"] = "@function.outer", ["[O"] = "@class.outer" },
-                    },
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        include_surrounding_whitespace = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ao"] = "@class.outer",
-                            ["io"] = "@class.inner",
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = { ["<leader>a"] = "@parameter.inner" },
-                        swap_previous = { ["<leader>A"] = "@parameter.inner" },
-                    },
-                },
-                ident = { enable = true },
-                rainbow = {
-                    enable = true,
-                    extended_mode = true,
-                    max_file_lines = nil,
-                },
+            local ts = require("nvim-treesitter")
+
+            local parsers = {
+                "python",
+                "lua",
+                "c",
+                "cpp",
+                "vim",
+                "json",
+                "toml",
+                "yaml",
+                "rust",
+                "bitbake",
+                "cmake",
+                "bash",
+                "kotlin",
+                "go",
+                "svelte",
+                "css",
+                "html",
+                "typescript",
+                "javascript",
+                "dart",
+            }
+
+            for _, parser in ipairs(parsers) do
+                pcall(ts.install, parser)
+            end
+
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
             })
-            require("treesitter-context").setup({})
         end,
         cmd = {
             "TSInstall",
@@ -80,12 +54,6 @@ return {
             "TSInstallInfo",
             "TSInstallSync",
             "TSInstallFromGrammar",
-        },
-        event = "User FileOpened",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-context",
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            "JoosepAlviste/nvim-ts-context-commentstring",
         },
     },
 }
