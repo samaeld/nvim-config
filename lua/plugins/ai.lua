c6e39e577b82ee7f776c3433cb0cdf192d0bfc20
Yurii Bilous <seidzuro@gmail.com>
2026-02-10 00:10:43 +0200

    ai: add claudecode

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -20,36 +20,13 @@ return {
             })
         end,
     },
-    -- {
-    --     "github/copilot.vim"
-    -- }
     {
-        "NickvanDyke/opencode.nvim",
+        "greggh/claude-code.nvim",
         dependencies = {
-            { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+            "nvim-lua/plenary.nvim",
         },
         config = function()
-            vim.g.opencode_opts = {}
-            vim.o.autoread = true
-
-            vim.keymap.set({ "n", "x" }, "<C-a>", function()
-                require("opencode").ask("@this: ", { submit = true })
-            end, { desc = "Ask opencode" })
-            vim.keymap.set({ "n", "x" }, "<C-x>", function()
-                require("opencode").select()
-            end, { desc = "Execute opencode action…" })
-            vim.keymap.set({ "n", "x" }, "ga", function()
-                require("opencode").prompt("@this")
-            end, { desc = "Add to opencode" })
-            vim.keymap.set({ "n", "t" }, "<C-.>", function()
-                require("opencode").toggle()
-            end, { desc = "Toggle opencode" })
-            vim.keymap.set("n", "<S-C-u>", function()
-                require("opencode").command("session.half.page.up")
-            end, { desc = "opencode half page up" })
-            vim.keymap.set("n", "<S-C-d>", function()
-                require("opencode").command("session.half.page.down")
-            end, { desc = "opencode half page down" })
+            require("claude-code").setup()
         end,
     },
 }
