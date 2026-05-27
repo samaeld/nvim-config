df26e8d967b718802a43b0bcff71c9c6d6aa70ae
Yurii Bilous <seidzuro@gmail.com>
2026-05-27 21:16:29 +0300

    add ts plugin

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "nemanjamalesija/ts-expand-hover.nvim",
+        ft = { "typescript", "typescriptreact" },
+        opts = {
+            keymaps = { hover = "<leader>th" },
+        },
+    },
+}
